ba3aa8b1ed8c3db3aee17c88e3e9a8a233557410
Fasih05 <fasihurrehman005@gmail.com>
2025-05-16 19:44:29 +0400

    Update car-details.js

--- js/car-details.js
+++ js/car-details.js
@@ -119,7 +119,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 background-color: rgba(0, 0, 0, 0.5);
                 border-radius: 50%;
                 width: 50px;
-                height: 50px;
+                height: 50px; 
                 display: flex;
                 align-items: center;
                 justify-content: center;
